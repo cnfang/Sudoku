254ab4c0b1dba60dbdaa4ad4168bae7fb40213ce
Jenny Fang <fang1992119@gmail.com>
2019-11-05 16:19:45 +1100

    add a test using hamcrest assertThat

--- src/test/java/idv/cnfang/Sodoku/CellTest.java
+++ src/test/java/idv/cnfang/Sodoku/CellTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 class CellTest {
 
     Cell cell;
@@ -30,6 +33,14 @@ class CellTest {
     void testGetCol() {
         System.out.println("col test");
         assertEquals(5, cell.getCol());
+    }
+    
+    /**
+     * an example of using hamcrest framework
+     */
+    @Test
+    void testHamcrestNotNullValue() {
+        assertThat(cell.getCol(), notNullValue());
     }
 
 }
